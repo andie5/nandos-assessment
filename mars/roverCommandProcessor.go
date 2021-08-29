653270f2833440b20dd4291c4ec4e3848ebd2dfe
andie5 <andieasa@googlemail.com>
2021-08-29 02:45:46 +0100

    add rover command processing functions

---
+++ mars/roverCommandProcessor.go
@@ -0,0 +1,49 @@
+package mars
+
+// DirectionToMove maps the direction to the positive or negative move of 1 or -1
+var DirectionToMove = map[string]int{
+	"N": 1,
+	"E": 1,
+	"S": -1,
+	"W": -1,
+}
+
+// rotateLeft updates the rover position 90 degrees in a anti-clockwise position N, E, S or W
+func (rover *RoverStatus) rotateLeft() {
+	anticlockwiseOrientation := map[string]string{
+		"N": "W",
+		"W": "S",
+		"S": "E",
+		"E": "N",
+	}
+	rover.Direction = anticlockwiseOrientation[rover.Direction]
+}
+
+// rotateRight updates the rover position 90 degrees in a clockwise position N, E, S or W
+func (rover *RoverStatus) rotateRight() {
+	clockwiseOrientation := map[string]string{
+		"N": "E",
+		"W": "N",
+		"S": "W",
+		"E": "S",
+	}
+	rover.Direction = clockwiseOrientation[rover.Direction]
+}
+
+// moveRover updates the rover x or y position based on the direction
+func (rover *RoverStatus) moveRover() {
+	move := DirectionToMove[rover.Direction]
+	if rover.Direction == "N" || rover.Direction == "S" {
+		rover.Y += move
+	} else if rover.Direction == "E" || rover.Direction == "W" {
+		rover.X += move
+	}
+}
+
+// validateMove checks that the rover's latest move position is within the grid axis coordinates and returns true if it is and false otherwise
+func (rover *RoverStatus) validateMove(grid PlanetAxis) bool {
+	if (rover.X <= grid.X && rover.X >= 0) && (rover.Y <= grid.Y && rover.Y >= 0) {
+		return true
+	}
+	return false
+}
---
+++ mars/roverCommandProcessor_test.go
@@ -0,0 +1,233 @@
+package mars
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRoverStatus_rotateLeft(t *testing.T) {
+	type fields struct {
+		X         int
+		Y         int
+		Direction string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   RoverStatus
+	}{
+		{
+			name:   "Default rover state with no direction if original rover is empty",
+			fields: fields{},
+			want:   RoverStatus{X: int(0), Y: int(0)},
+		},
+		{
+			name: "Updates rover with the direction updated 90 degrees anticlockwise",
+			fields: fields{
+				X:         int(2),
+				Y:         int(3),
+				Direction: "N",
+			},
+			want: RoverStatus{X: int(2), Y: int(3), Direction: "W"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rover := RoverStatus{
+				X:         tt.fields.X,
+				Y:         tt.fields.Y,
+				Direction: tt.fields.Direction,
+			}
+			rover.rotateLeft()
+
+			// Check rover status object after update
+			if !reflect.DeepEqual(rover, tt.want) {
+				t.Errorf("rotateLeft() = %v, want %v", rover, tt.want)
+			}
+		})
+	}
+}
+
+func TestRoverStatus_rotateRight(t *testing.T) {
+	type fields struct {
+		X         int
+		Y         int
+		Direction string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   RoverStatus
+	}{
+		{
+			name:   "Default rover state with no direction if original rover is empty",
+			fields: fields{},
+			want:   RoverStatus{X: int(0), Y: int(0)},
+		},
+		{
+			name: "Updates rover with the direction updated 90 degrees clockwise",
+			fields: fields{
+				X:         int(2),
+				Y:         int(3),
+				Direction: "E",
+			},
+			want: RoverStatus{X: int(2), Y: int(3), Direction: "S"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rover := RoverStatus{
+				X:         tt.fields.X,
+				Y:         tt.fields.Y,
+				Direction: tt.fields.Direction,
+			}
+			rover.rotateRight()
+
+			// Check rover status object after update
+			if !reflect.DeepEqual(rover, tt.want) {
+				t.Errorf("rotateLeft() = %v, want %v", rover, tt.want)
+			}
+		})
+	}
+}
+
+func TestRoverStatus_moveRover(t *testing.T) {
+	type fields struct {
+		X         int
+		Y         int
+		Direction string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   RoverStatus
+	}{
+		{
+			name:   "Default rover state with no direction if original rover is empty",
+			fields: fields{},
+			want:   RoverStatus{X: int(0), Y: int(0)},
+		},
+		{
+			name: "Updates rover with one move west, in the x direction i.e -1 to current position",
+			fields: fields{
+				X:         int(1),
+				Y:         int(3),
+				Direction: "W",
+			},
+			want: RoverStatus{X: int(0), Y: int(3), Direction: "W"},
+		},
+		{
+			name: "Updates rover with one move north, in the y direction i.e +1 to current position",
+			fields: fields{
+				X:         int(1),
+				Y:         int(3),
+				Direction: "N",
+			},
+			want: RoverStatus{X: int(1), Y: int(4), Direction: "N"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rover := RoverStatus{
+				X:         tt.fields.X,
+				Y:         tt.fields.Y,
+				Direction: tt.fields.Direction,
+			}
+			rover.moveRover()
+
+			// Check rover status object after update
+			if !reflect.DeepEqual(rover, tt.want) {
+				t.Errorf("rotateLeft() = %v, want %v", rover, tt.want)
+			}
+		})
+	}
+}
+
+func TestRoverStatus_validateMove(t *testing.T) {
+	type fields struct {
+		X         int
+		Y         int
+		Direction string
+	}
+	type args struct {
+		grid PlanetAxis
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			name: "Returns true if rover coordinates is 0,0 which is within the planet grid axis",
+			fields: fields{
+				X:         int(0),
+				Y:         int(0),
+				Direction: "N",
+			},
+			args: args{
+				grid: PlanetAxis{X: int(3), Y: int(1)},
+			},
+			want: true,
+		},
+		{
+			name: "Returns true if rover coordinates is within the planet grid axis",
+			fields: fields{
+				X:         int(1),
+				Y:         int(3),
+				Direction: "N",
+			},
+			args: args{
+				grid: PlanetAxis{X: int(3), Y: int(3)},
+			},
+			want: true,
+		},
+		{
+			name: "Returns true if rover coordinates if one coordinate is within the planet grid axis and other coordinate is not provided i.e. 0",
+			fields: fields{
+				X:         int(1),
+				Direction: "N",
+			},
+			args: args{
+				grid: PlanetAxis{X: int(3), Y: int(1)},
+			},
+			want: true,
+		},
+		{
+			name: "Returns false if rover coordinates is not within the planet grid axis",
+			fields: fields{
+				X:         int(1),
+				Y:         int(3),
+				Direction: "N",
+			},
+			args: args{
+				grid: PlanetAxis{X: int(3), Y: int(1)},
+			},
+			want: false,
+		},
+		{
+			name: "Returns false if planet grid axis is not supplied and the rover coordinates is not 0,0",
+			fields: fields{
+				X:         int(1),
+				Y:         int(3),
+				Direction: "N",
+			},
+			args: args{
+				grid: PlanetAxis{},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rover := &RoverStatus{
+				X:         tt.fields.X,
+				Y:         tt.fields.Y,
+				Direction: tt.fields.Direction,
+			}
+			if got := rover.validateMove(tt.args.grid); got != tt.want {
+				t.Errorf("RoverStatus.validateMove() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

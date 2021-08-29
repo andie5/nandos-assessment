39ce35ccc6e84accb9e252553029942a0324b256
andie5 <andieasa@googlemail.com>
2021-08-29 02:55:34 +0100

    add func to print rover outputs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"nandos/marsrover/v1/mars"
 )
@@ -24,7 +25,18 @@ func main() {
 		log.Fatalf("Error processing initial planet axis input: %v error: %v", fileOutput, err)
 	}
 
+	//temp initialise rover object
+	rovers := map[int]mars.RoverStatus{}
+	printRoverPositions(rovers)
+
 	log.Println("filename: ", fileOutput)
 	log.Println("planetGrid: ", planetGrid)
 
+}
+
+// printRoverPositions prints the rover coordinates and direction
+func printRoverPositions(rovers map[int]mars.RoverStatus) {
+	for _, rover := range rovers {
+		fmt.Printf("%v %v %v\n", rover.X, rover.Y, rover.Direction)
+	}
 }
